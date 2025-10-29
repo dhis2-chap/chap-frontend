4cca00427fab06387d02e1f21011bd79ebccd595
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-10-29 13:51:41 +0100

    chore: update types

--- packages/ui/src/httpfunctions/models/DatasetCreate.ts
+++ packages/ui/src/httpfunctions/models/DatasetCreate.ts
@@ -6,8 +6,17 @@ import type { DataSource } from './DataSource';
 import type { FeatureCollectionModel } from './FeatureCollectionModel';
 import type { ObservationBase } from './ObservationBase';
 export type DatasetCreate = {
+    /**
+     * Name of dataset
+     */
     name: string;
+    /**
+     * A mapping of covariate names to data element IDs from which to source the data
+     */
     dataSources?: (Array<DataSource> | null);
+    /**
+     * Purpose of dataset, e.g., 'forecasting' or 'backtesting'
+     */
     type?: (string | null);
     observations: Array<ObservationBase>;
     geojson: FeatureCollectionModel;
--- packages/ui/src/httpfunctions/models/MakeBacktestWithDataRequest.ts
+++ packages/ui/src/httpfunctions/models/MakeBacktestWithDataRequest.ts
@@ -11,7 +11,13 @@ export type MakeBacktestWithDataRequest = {
     nSplits: number;
     stride: number;
     name: string;
+    /**
+     * A mapping of covariate names to data element IDs from which to source the data
+     */
     dataSources?: (Array<DataSource> | null);
+    /**
+     * Purpose of dataset, e.g., 'forecasting' or 'backtesting'
+     */
     type?: (string | null);
     geojson: FeatureCollectionModel;
     providedData: Array<ObservationBase>;
