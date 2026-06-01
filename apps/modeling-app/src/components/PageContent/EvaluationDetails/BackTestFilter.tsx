1ce2ac7b11662331f52de0ea9ed19d5db67ef665
yambanso <yambanso22@gmail.com>
2026-06-01 11:55:22 +0200

    custom filter

---
+++ apps/modeling-app/src/components/PageContent/EvaluationDetails/BackTestFilter.tsx
@@ -0,0 +1,12 @@
+type Props = {
+    split_periods: string[];
+    locations: string[];
+}
+
+export const BackTestFilter = ({ split_periods, locations }: Props) => {
+    return (
+        <div>
+            Something here
+        </div>
+    );
+}
